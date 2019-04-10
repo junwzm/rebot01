4aae505b8bfb453269d63689bd2c8a07f6630e99
junwzm <675856306@qq.com>
2019-04-10 14:57:21 +0800

    地图搭建，碰撞识别

--- ui/view.py
+++ ui/view.py
@@ -3,67 +3,69 @@ from ui.locals import *
 from ui.action import *
 
 
-class PlayerTank(Display,Move):
-    def __init__(self,**kwargs):
-        self.x=kwargs['x']
-        self.y=kwargs['y']
-        self.imges=[
+class PlayerTank(Display, Move):
+    def __init__(self, **kwargs):
+        self.x = kwargs['x']
+        self.y = kwargs['y']
+        self.imges = [
             pygame.image.load("img/p1tankU.gif"),
             pygame.image.load("img/p1tankD.gif"),
             pygame.image.load("img/p1tankL.gif"),
             pygame.image.load("img/p1tankR.gif")
         ]
-        self.direction=Direction.UP
-        self.surface=kwargs['surface']
-        self.speed=5
-        self.bad_direction=Direction.NONE
-        self.width=self.imges[0].get_width()
-        self.height=self.imges[0].get_height()
-    def display(self):
-        if self.direction==Direction.UP:
-            self.surface.blit(self.imges[0],(self.x,self.y))
-        if self.direction==Direction.DOWN:
-            self.surface.blit(self.imges[1],(self.x,self.y))
-        if self.direction==Direction.LEFT:
-            self.surface.blit(self.imges[2],(self.x,self.y))
-        if self.direction==Direction.RIGHT:
-            self.surface.blit(self.imges[3],(self.x,self.y))
-    def move(self,direction):
+        self.direction = Direction.UP
+        self.surface = kwargs['surface']
+        self.speed = 5
+        self.bad_direction = Direction.NONE
+        self.width = self.imges[0].get_width()
+        self.height = self.imges[0].get_height()
 
-        if direction==self.bad_direction:
+    def display(self):
+        if self.direction == Direction.UP:
+            self.surface.blit(self.imges[0], (self.x, self.y))
+        if self.direction == Direction.DOWN:
+            self.surface.blit(self.imges[1], (self.x, self.y))
+        if self.direction == Direction.LEFT:
+            self.surface.blit(self.imges[2], (self.x, self.y))
+        if self.direction == Direction.RIGHT:
+            self.surface.blit(self.imges[3], (self.x, self.y))
+
+    def move(self, direction):
+
+        if direction == self.bad_direction:
             return
 
-        if self.direction!=direction:
-            self.direction=direction
+        if self.direction != direction:
+            self.direction = direction
 
 
 
         else:
-            if direction==Direction.UP:
-                self.y-=self.speed
+            if direction == Direction.UP:
+                self.y -= self.speed
                 if self.y < 0:
                     self.y = 0
-            if direction==Direction.DOWN:
-                self.y+=self.speed
+            if direction == Direction.DOWN:
+                self.y += self.speed
                 if self.y > GAME_HEIGHT - self.height:
                     self.y = GAME_HEIGHT - self.height
-            if direction==Direction.LEFT:
-                self.x-=self.speed
+            if direction == Direction.LEFT:
+                self.x -= self.speed
                 if self.x < 0:
                     self.x = 0
 
-            if direction==Direction.RIGHT:
-                self.x+=self.speed
+            if direction == Direction.RIGHT:
+                self.x += self.speed
                 if self.x > GAME_WIDTH - self.width:
                     self.x = GAME_WIDTH - self.width
 
-
     def fire(self):
         pass
-    def is_blocked(self,block):
 
-        next_x=self.x
-        next_y=self.y
+    def is_blocked(self, block):
+
+        next_x = self.x
+        next_y = self.y
         if self.direction == Direction.UP:
             next_y -= self.speed
             if next_y < 0:
@@ -72,7 +74,7 @@ class PlayerTank(Display,Move):
 
         if self.direction == Direction.DOWN:
             next_y += self.speed
-            if next_y >GAME_HEIGHT-self.height:
+            if next_y > GAME_HEIGHT - self.height:
                 self.bad_direction = self.direction
                 return True
         if self.direction == Direction.LEFT:
@@ -82,39 +84,39 @@ class PlayerTank(Display,Move):
                 return True
         if self.direction == Direction.RIGHT:
             next_x += self.speed
-            if next_x > GAME_WIDTH-self.width:
+            if next_x > GAME_WIDTH - self.width:
                 self.bad_direction = self.direction
                 return True
 
-        rect_player=pygame.Rect(next_x,next_y,self.width,self.height)
-        rect_wall=pygame.Rect(block.x,block.y,block.width,block.height)
+        rect_player = pygame.Rect(next_x, next_y, self.width, self.height)
+        rect_wall = pygame.Rect(block.x, block.y, block.width, block.height)
 
-        collide=pygame.Rect.colliderect(rect_player,rect_wall)
+        collide = pygame.Rect.colliderect(rect_player, rect_wall)
         if collide:
-            self.bad_direction=self.direction
+            self.bad_direction = self.direction
             return True
         else:
-            self.bad_direction=Direction.NONE
+            self.bad_direction = Direction.NONE
             return False
 
 
+class Wall(Display, Block):
+    def __init__(self, **kwargs):
+        self.x = kwargs['x']
+        self.y = kwargs['y']
+        self.surface = kwargs['surface']
+        self.imge = pygame.image.load("img/walls.gif")
+        self.width = self.imge.get_width()
+        self.height = self.imge.get_height()
 
-
-class Wall(Display,Block):
-    def __init__(self,**kwargs):
-        self.x=kwargs['x']
-        self.y=kwargs['y']
-        self.surface=kwargs['surface']
-        self.imge=pygame.image.load("img/walls.gif")
-        self.width=self.imge.get_width()
-        self.height=self.imge.get_height()
     def display(self):
-        self.surface.blit(self.imge,(self.x,self.y))
-
+        self.surface.blit(self.imge, (self.x, self.y))
 
     def destroy(self):
         pass
-class Steel(Display,Block):
+
+
+class Steel(Display, Block):
     def __init__(self, **kwargs):
         self.x = kwargs['x']
         self.y = kwargs['y']
@@ -126,7 +128,8 @@ class Steel(Display,Block):
     def display(self):
         self.surface.blit(self.imge, (self.x, self.y))
 
-class Water(Display,Block):
+
+class Water(Display, Block):
     def __init__(self, **kwargs):
         self.x = kwargs['x']
         self.y = kwargs['y']
